a3cb90dc73aa9e386ecec87b7d0d33b45313622f
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-06-23 16:01:17 +0300

    Add backend/src/routes/allocationRoutes.js

---
+++ backend/src/routes/allocationRoutes.js
@@ -0,0 +1,3 @@
+﻿const express = require('express'); const r = express.Router(); const c = require('../controllers/allocationController');
+r.get('/', c.getAll); r.post('/', c.create); r.put('/:id/approve', c.approve); r.put('/:id/reject', c.reject); r.put('/:id/disburse', c.disburse);
+module.exports = r;
