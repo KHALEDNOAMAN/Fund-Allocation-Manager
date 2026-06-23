042f52066ce9421309b30a80a9034faf55249a60
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-06-23 16:01:21 +0300

    Add backend/src/routes/reportRoutes.js

---
+++ backend/src/routes/reportRoutes.js
@@ -0,0 +1,3 @@
+﻿const express = require('express'); const r = express.Router(); const c = require('../controllers/reportController');
+r.get('/summary', c.fundSummary); r.get('/variance', c.budgetVariance); r.get('/allocations', c.allocationsByPriority);
+module.exports = r;
