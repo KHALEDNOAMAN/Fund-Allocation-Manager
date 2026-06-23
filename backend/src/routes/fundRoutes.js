65164e7cd41d207c670256f79261b824d2974e48
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-06-23 16:01:19 +0300

    Add backend/src/routes/fundRoutes.js

---
+++ backend/src/routes/fundRoutes.js
@@ -0,0 +1,3 @@
+﻿const express = require('express'); const r = express.Router(); const c = require('../controllers/fundController');
+r.get('/', c.getAll); r.get('/:id', c.getById); r.post('/', c.create); r.put('/:id', c.update);
+module.exports = r;
