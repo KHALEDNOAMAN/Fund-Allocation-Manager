eb5540759c2dc7fec46346148df8b182ce93ef41
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-06-23 16:01:30 +0300

    Add frontend/src/App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import FundList from './components/FundList';
+import AllocationForm from './components/AllocationForm';
+import TransferForm from './components/TransferForm';
+export default function App() {
+  return (<BrowserRouter><Routes>
+    <Route path="/" element={<Navigate to="/funds/dashboard" />} />
+    <Route path="/funds/dashboard" element={<Dashboard />} />
+    <Route path="/funds/list" element={<FundList />} />
+    <Route path="/funds/allocations/new" element={<AllocationForm />} />
+    <Route path="/funds/transfers/new" element={<TransferForm />} />
+  </Routes></BrowserRouter>);
+}
