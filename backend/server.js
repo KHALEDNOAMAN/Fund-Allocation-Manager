f120a865fcc4fc28b947f17ab538f76b06162919
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-06-23 16:00:54 +0300

    Add backend/server.js

---
+++ backend/server.js
@@ -0,0 +1,3 @@
+﻿require('dotenv').config();
+const app = require('./src/app');
+app.listen(process.env.PORT || 3006, () => console.log(`Fund Allocation API running on port ${process.env.PORT || 3006}`));
